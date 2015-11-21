cbaa02a09e957e153bf3132b0f9034af997ffbe8
insanegrv <insanegrv@gmail.com>
2015-11-21 17:46:26 +0300

    Edits from tutor

--- js/check.js
+++ js/check.js
@@ -1,26 +1,31 @@
+
 function getMessage(a, b) {
-  if (a === true) {
-    return 'Я попал в ' + b;
-  } else if (a === false) {
-    return 'Я никуда не попал';
-  }
 
-  if (typeof a === 'number') {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Я попал в ' + b;
+    } else {
+      return 'Я никуда не попал';
+    }
+
+  } else if (typeof a === 'number') {
     return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
-  }
 
-  if (Array.isArray(a) && !Array.isArray(b)) {
-    var sum = a.reduce(function(temp, current) {
+  } else if (Array.isArray(a)) {
+    if (Array.isArray(b)) {
+      var length = a.reduce(function(temp, current, number) {
+        return temp + current * b[number];
+      }, 0);
+      return 'Я прошёл ' + length + ' метров';
+    } else {
+      var sum = a.reduce(function(temp, current) {
       return temp + current;
-    });
-    return 'Я прошёл ' + sum + ' шагов';
-  }
+    }, 0);
+      return 'Я прошёл ' + sum + ' шагов';
+    }
 
-  if (Array.isArray(a) && Array.isArray(b)) {
-    var length = a.reduce(function(temp, current, number) {
-      return temp + current * b[number];
-    });
-    return 'Я прошёл ' + length + ' метров';
+  } else {
+    throw 'Неверные аргументы';
   }
 }
 
