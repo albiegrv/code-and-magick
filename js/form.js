1dcead75957a3216f15775413e6e1d70cae41cea
insanegrv <insanegrv@gmail.com>
2015-11-30 05:04:09 +0300

    Validation. Edits from tutor

--- js/form.js
+++ js/form.js
@@ -21,12 +21,38 @@
     evt.preventDefault();
     formContainer.classList.remove('invisible');
 
+    validateForm();
+  };
+
+  // Закрываем форму
+  formCloseButton.onclick = function(evt) {
+    evt.preventDefault();
+    formContainer.classList.add('invisible');
+  };
+
+  // Событие - изменение поля ОЦЕНКА
+  markParent.onchange = function() {
+    validateForm();
+  }
+
+  // Событие - изменение поля ИМЯ
+  name.onkeyup = function() {
+    validateForm();
+  }
+
+  // Событие - изменение поля ОТЗЫВ
+  text.onkeyup = function() {
+    validateForm();
+  }
+
+  // Ф-ция валидации формы
+  function validateForm() {
     // Проверка полей с ОЦЕНКОЙ и ОТЗЫВОМ
     // Находим выбранную оценку
     var mark = form.querySelector('[name=review-mark]:checked');
     // Если оценка ниже 3, поле с отзывом становится
     // обязательным
-    if (mark.value < 3) {
+    if (Number(mark.value) < 3) {
       text.setAttribute('required', '');
       // Прячем или показываем подсказку
       // в зависимости от заполненности поля
@@ -67,90 +93,6 @@
         hintParent.classList.add('invisible');
         submit.removeAttribute('disabled');
       } else {
-        hintParent.classList.remove('invisible');
-        submit.setAttribute('disabled', '');
-      }
-    }
-  };
-
-  // Закрываем форму
-  formCloseButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.add('invisible');
-  };
-
-  // Событие - изменение поля ОЦЕНКА
-  markParent.onchange = function() {
-    // Находим выбранную оценку
-    var mark = form.querySelector('[name=review-mark]:checked');
-
-    if (mark.value < 3) {
-      text.setAttribute('required', '');
-      // Прячем или показываем подсказку
-      // В зависимости от заполненности поля
-      if (text.value == '') {
-        textHint.classList.remove('invisible');
-        hintParent.classList.remove('invisible');
-        submit.setAttribute('disabled', '');
-      } else {
-        textHint.classList.add('invisible');
-        // Если поле "имя" валидно, то
-        // прячем весь блок с подсказками и активируем кнопку
-        if (nameHint.classList.contains('invisible')) {
-          hintParent.classList.add('invisible');
-          submit.removeAttribute('disabled');
-        } else {
-          // Если не валидно, возвращаем подсказки
-          // и дизейблим кнопку
-          hintParent.classList.remove('invisible');
-          submit.setAttribute('disabled', '');
-        }
-      }
-    // Если оценка 3 или выше, то отзыв необязателен
-    } else {
-      text.removeAttribute('required');
-      textHint.classList.add('invisible');
-    }
-  }
-
-  // Событие - изменение поля ИМЯ
-  name.onchange = function() {
-    if (name.value == '') {
-      nameHint.classList.remove('invisible');
-      hintParent.classList.remove('invisible');
-      submit.setAttribute('disabled', '');
-    } else {
-      nameHint.classList.add('invisible');
-      // Если поле "отзыв" валидно, то
-      // прячем весь блок с подсказками и активируем кнопку
-      if (textHint.classList.contains('invisible')) {
-        hintParent.classList.add('invisible');
-        submit.removeAttribute('disabled');
-      } else {
-        // Если не валидно, возвращаем подсказки
-        // и дизейблим кнопку
-        hintParent.classList.remove('invisible');
-        submit.setAttribute('disabled', '');
-      }
-    }
-  }
-
-  // Событие - изменение поля ОТЗЫВ
-  text.onchange = function() {
-    if (text.hasAttribute('required') && text.value == '') {
-      textHint.classList.remove('invisible');
-      hintParent.classList.remove('invisible');
-      submit.setAttribute('disabled', '');
-    } else {
-      textHint.classList.add('invisible');
-      // Если поле "имя" валидно, то
-      // прячем весь блок с подсказками и активируем кнопку
-      if (nameHint.classList.contains('invisible')) {
-        hintParent.classList.add('invisible');
-        submit.removeAttribute('disabled');
-      } else {
-        // Если не валидно, возвращаем подсказки
-        // и дизейблим кнопку
         hintParent.classList.remove('invisible');
         submit.setAttribute('disabled', '');
       }
